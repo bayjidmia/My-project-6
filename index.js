553174f0ef86ef964068b318d4e5a856dcf848a9
bayjidmia <mdbayjidmia982@gmail.com>
2025-09-09 20:51:07 +0600

    added the modified js file and readme file and modified html file

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const displaycatagory =(posts)=>{
 
     const displayplants =(plants)=>{
           
-         const display =document.getElementById('card')
+         const display =document.getElementById('card1')
           display.innerHTML='';
 
           for(const plant of plants){
@@ -45,13 +45,13 @@ const displaycatagory =(posts)=>{
         <div class="h-40 bg-gray-200 rounded-md flex items-center justify-center mb-4">
           <img src="${plant.image}" alt="${plant.name}" class="max-h-full w-full object-cover rounded-md">
         </div>
-        <h3 class="text-lg font-semibold mb-1">${plant.name}</h3>
+        <h3 onclick="showdetails(${plant.id})" class="text-lg font-semibold mb-1">${plant.name}</h3>
         <p class="text-sm text-gray-600 mb-2 h-28">${plant.description}</p>
         <div class="flex items-center justify-between mb-5">
           <span class="text-xs bg-green-100 text-green-700 px-2 py-1  rounded-full">${plant.category}</span>
           <span class="font-semibold">৳${plant.price}</span>
         </div>
-        <button class="w-full bg-green-700 hover:bg-green-800 text-white py-2 rounded-lg">
+        <button onclick="addToHistory('${plant.name}', '${plant.price}')" class="w-full bg-green-700 hover:bg-green-800 text-white py-2 rounded-lg">
           Add to Cart
         </button>
       </div>
@@ -78,7 +78,7 @@ const displaycatagory =(posts)=>{
 
     const displayallplants =(alls)=>{
      
-        const displayt =document.getElementById('card')
+        const displayt =document.getElementById('card1')
           displayt.innerHTML='';
 
           
@@ -90,30 +90,124 @@ const displaycatagory =(posts)=>{
         <div class="h-40 bg-gray-200 rounded-md flex items-center justify-center mb-4">
           <img src="${all.image}" alt="${all.name}" class="max-h-full w-full object-cover rounded-md">
         </div>
-        <h3 class="text-lg font-semibold mb-1">${all.name}</h3>
+        <h3 onclick="showdetails(${all.id})" class="text-lg font-semibold mb-1">${all.name}</h3>
         <p class="text-sm text-gray-600 mb-2 h-28">${all.description}</p>
         <div class="flex items-center justify-between mb-5">
           <span class="text-xs bg-green-100 text-green-700 px-2 py-1  rounded-full">${all.category}</span>
           <span class="font-semibold">৳${all.price}</span>
         </div>
-        <button class="w-full bg-green-700 hover:bg-green-800 text-white py-2 rounded-lg">
+        <button onclick="addToHistory('${all.name}', '${all.price}')" class="w-full bg-green-700 hover:bg-green-800 text-white py-2 rounded-lg">
           Add to Cart
         </button>
-      </div>
-                
-            `
+      </div>`
+                 displayt.append(plantdivs)
 
-            displayt.append(plantdivs)
-
-
+         
 
       }}
 
     allloadpost()
     
 
+    let totaltrees =0;
+    let total =0;
+    const historyCount=[];
+   const  addToHistory =(name,price)=>{
+     totaltrees=1;
+     
+     total=Number(total) +Number(price)
+        
+    const history =document.getElementById('history')
+
+    
+    const historydiv =document.createElement('div')
+
+    historydiv.innerHTML=`
+      <div class="card bg-[#F0FDF4] flex justify-between h-[100px] flex-row ml-2 mr-2 mt-3">
+       
+       <div class="flex flex-col justify-center items-center text-center ml-2">
+         <h1 class="font-bold">'${name}'</h1>
+          <div class="flex">
+            <p>${price}<p>
+           <p><i class="fa-solid fa-xmark"></i></p>
+             <p>${totaltrees}</p>
+          </div>
+       </div>
+        <div class=" flex items-center text-center mr-2"><button class="clear-btn"><i class="fa-solid fa-xmark text-gray-300"></i></button></div>
+    </div>
+      <div class="history2 border-t border-gray-400 mt-3">
+          <div>
+            <div class="flex gap-3 justify-end mr-3">
+              <h1>total</h1>
+              <p>=</p>
+               <p id='total-value'>${total}</p>
+            </div>
+          </div>
+        </div>
+    
+    
+    `
+
+    history.appendChild(historydiv)
+
+   }
+
+
+
+
+
+document.addEventListener('click', function (e){
+  if (e.target.closest('.clear-btn')) {
+    
+    e.target.closest('.card').remove();}})
+    
+  
+
+
+ const showdetails=(id)=>{
+   fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
+   .then(res=>res.json())
+   .then(json=>displaydetals(json.plants))
+ }
+
+  const displaydetals =(ip)=>{
+
+     const container = document.getElementById('details-container');
+      const content = document.getElementById('details-content');
+
+      
+      content.innerHTML = '';
+
+      
+      const card = document.createElement('div');
+      card.innerHTML = `
+        
+        <img src="${ip.image}" alt="${ip.name}" class="w-full mb-2 rounded">
+         <h2 class="text-xl font-bold mb-2">${ip.name}</h2>
+        <p>${ip.description}</p>
+        <p class="mt-2 font-semibold">৳${ip.price}</p>
+      `;
+
+      content.appendChild(card);
+
+      
+      container.classList.remove('hidden');
 
    
+const closeBtn = document.getElementById('close-btn');
+const detailsContainer = document.getElementById('details-container');
+
+closeBtn.addEventListener('click', () => {
+  detailsContainer.classList.add('hidden'); 
+});
+
+
+
+  }
+  
+
+         
+ 
 
 
 
@@ -124,3 +218,6 @@ const displaycatagory =(posts)=>{
 loadcatagory()
 
  
+
+
+
